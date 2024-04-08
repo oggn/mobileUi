074df12d16efeea9011253520b52233a041da280
jeong-jae-hwan <jeahwani03@gmail.com>
2024-04-08 13:57:15 +0900

    4.4.5

--- src/_ui/flex/P.tsx
+++ src/_ui/flex/P.tsx
@@ -1,22 +1,18 @@
-import React, { Children, ReactElement, cloneElement } from 'react';
-import { Sticky } from './position/Sticky';
-import { Fixed } from './position/Fixed';
-import { Absolute } from './position/Absolute';
-import { BottomFixed } from './position/BottomFixed';
-import { BottomFixedAnimate } from './position/BottomFixedAnimate';
+import React, { Children, ReactElement, cloneElement } from 'react'
+import { Sticky } from './position/Sticky'
+import { Fixed } from './position/Fixed'
+import { Absolute } from './position/Absolute'
+import { BottomFixed } from './position/BottomFixed'
+import { BottomFixedAnimate } from './position/BottomFixedAnimate'
 
 export function P({ children }: { children: ReactElement }) {
-  const child = Children.only(children);
+    const child = Children.only(children)
 
-  return (
-    <div css={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-      {cloneElement(child, { ...child.props })}
-    </div>
-  );
+    return cloneElement(child, { ...child.props })
 }
 
-P.Sticky = Sticky;
-P.Fixed = Fixed;
-P.Absolute = Absolute;
-P.BottomFixed = BottomFixed;
-P.BottomFixedAnimate = BottomFixedAnimate;
+P.Sticky = Sticky
+P.Fixed = Fixed
+P.Absolute = Absolute
+P.BottomFixed = BottomFixed
+P.BottomFixedAnimate = BottomFixedAnimate
